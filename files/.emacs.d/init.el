5fce1a220b03efc53d857f45b252a03bfecaf5c0
Gonz Saavedra <gonzalosaavedra@gmail.com>
2013-07-09 12:57:50 -0300

    Added goto-symbol from prelude to init.el.

--- files/.emacs.d/init.el
+++ files/.emacs.d/init.el
@@ -285,6 +285,58 @@ point reaches the beginning or end of the buffer, stop there."
     (indent-for-tab-command)))
 
 
+;; Jump to symbol definition (uses ido)
+;; taken from prelude
+(defun goto-symbol (&optional symbol-list)
+  "Refresh imenu and jump to a place in the buffer using Ido."
+  (interactive)
+  (unless (featurep 'imenu)
+    (require 'imenu nil t))
+  (cond
+   ((not symbol-list)
+    (let ((ido-mode ido-mode)
+          (ido-enable-flex-matching
+           (if (boundp 'ido-enable-flex-matching)
+               ido-enable-flex-matching t))
+          name-and-pos symbol-names position)
+      (unless ido-mode
+        (ido-mode 1)
+        (setq ido-enable-flex-matching t))
+      (while (progn
+               (imenu--cleanup)
+               (setq imenu--index-alist nil)
+               (goto-symbol (imenu--make-index-alist))
+               (setq selected-symbol
+                     (ido-completing-read "Symbol? " symbol-names))
+               (string= (car imenu--rescan-item) selected-symbol)))
+      (unless (and (boundp 'mark-active) mark-active)
+        (push-mark nil t nil))
+      (setq position (cdr (assoc selected-symbol name-and-pos)))
+      (cond
+       ((overlayp position)
+        (goto-char (overlay-start position)))
+       (t
+        (goto-char position)))
+      (recenter)))
+   ((listp symbol-list)
+    (dolist (symbol symbol-list)
+      (let (name position)
+        (cond
+         ((and (listp symbol) (imenu--subalist-p symbol))
+          (goto-symbol symbol))
+         ((listp symbol)
+          (setq name (car symbol))
+          (setq position (cdr symbol)))
+         ((stringp symbol)
+          (setq name symbol)
+          (setq position
+                (get-text-property 1 'org-imenu-marker symbol))))
+        (unless (or (null position) (null name)
+                    (string= (car imenu--rescan-item) name))
+          (add-to-list 'symbol-names (substring-no-properties name))
+          (add-to-list 'name-and-pos (cons (substring-no-properties name) position))))))))
+
+
 ;;;;; key bindings
 
 ;; Basic movement key bindings
@@ -337,7 +389,8 @@ point reaches the beginning or end of the buffer, stop there."
 ;; Newline next/prev and indent
 (global-set-key (kbd "<C-return>") 'newline-next-and-indent)
 (global-set-key (kbd "<C-S-return>") 'newline-prev-and-indent)
-
+;; Go to symbol
+(global-set-key (kbd "M-j") 'goto-symbol)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
