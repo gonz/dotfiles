746412775a6468d4bff2f2bdb963f8c68bb78611
Gonz Saavedra <gonzalosaavedra@gmail.com>
2014-03-21 16:06:57 -0300

    Emacs nweb js-mode match fixes.

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -242,7 +242,7 @@
 ;; multi-web-mode
 (require 'multi-web-mode)
 (setq mweb-default-major-mode 'jinja2-mode)
-(setq mweb-tags '((js-mode "<script[^>]*>" "</script>")
+(setq mweb-tags '((js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
                   (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
 (setq mweb-filename-extensions '("htm" "html"))
 (multi-web-global-mode 1)
